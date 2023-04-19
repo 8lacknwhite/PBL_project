4bb0aa26fff9036e7c504f4799b7534af10ab00a
Nimit Kumar <nimitkumar2002@gmail.com>
2023-04-20 02:15:24 +0530

    made alot of changes frontend now is able to send json pakage to backend

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,35 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const { SageMakerClient, AddAssociationCommand } = require("@aws-sdk/client-sagemaker")
-const router = require('express').Router()
-app.use(bodyParser.json())
+const { SageMakerClient } = require("@aws-sdk/client-sagemaker")
 
-const client = new SageMakerClient({region: ""})
-
-router.post('/output', async (req,res) => {
-    try{
-        let param = await ser
+// create a new AWS SageMaker Runtime client
+const sagemaker = new AWS.SageMakerRuntime({
+    apiVersion: '2017-05-13',
+    region: 'us-east-1'
+  });
+  
+  // define the endpoint name of your deployed ML model on SageMaker
+  const endpointName = 'your-SageMaker-endpoint-name';
+  
+  // define your POST request handler function
+  exports.predictPrice = async (req, res) => {
+    // get the data points from the frontend
+    const { dataPoints } = req.body;
+  
+    try {
+      // send a POST request to the SageMaker endpoint with the data points
+      const response = await sagemaker.invokeEndpoint({
+        EndpointName: endpointName,
+        Body: JSON.stringify(dataPoints),
+        ContentType: 'application/json',
+        Accept: 'application/json'
+      }).promise();
+  
+      // parse the response from SageMaker and send it back to the frontend
+      const predictedPrice = JSON.parse(response.Body.toString('utf-8'));
+      res.json({ predictedPrice });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to predict price.' });
     }
-})
-
-
-
-app.listen(3000, () => {
-    console.log("Server started at port 3000")
-})
+  };
--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { carData } from "../data/Appdata";
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// year, transmission, km, fuel, modal, manufacturer, location, ownership, 
 
 function Homepage() {
   const [selectedManufacturer, setSelectedManufacturer] = useState('');
@@ -10,28 +11,72 @@ function Homepage() {
   const [year, setYear] = useState('');
   const [km, setKm] = useState('');
   const [selectedTransmission, setSelectedTransmission] = useState('');
+  const [selectedFuel, setSelectedFuel] = useState('');
+  const [selectedLocation, setSelectedLocation] = useState('');
+  const [selectedOwnership, setSelectedOwnership] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      manufacturer: selectedManufacturer,
+      model: selectedModel,
+      year: year,
+      distance: km,
+      transmission: selectedTransmission,
+      location: selectedLocation,
+      owenership: selectedOwnership
+    }
+    fetch('/api/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+
 
   const handleManufacturerSelect = (event) => {
     setSelectedManufacturer(event.target.value);
     setSelectedModel('');
     setSelectedTransmission('');
-  };
-
-  const handleTransmissionSelect = (event) => {
-    setSelectedTransmission(event.target.value);
+    setSelectedFuel('');
+    setSelectedOwnership('');
   };
   const handleModelSelect = (event) => {
     setSelectedModel(event.target.value);
     setSelectedTransmission('');
+    setSelectedFuel('');
+    setSelectedOwnership('');
   };
+  const handleTransmissionSelect = (event) => {
+    setSelectedTransmission(event.target.value);
+    setSelectedFuel('');
+    setSelectedOwnership('');
+  };
+  const handleFuelSelect = (event) => {
+    setSelectedFuel(event.target.value);
+    setSelectedOwnership('');
+  }
+  const handleOwnershipSelect = (event) => {
+    setSelectedOwnership(event.target.value);
+  }
   const handleYearInput = (event) => {
     const value = event.target.value.replace(/\D/g, ''); // only allow digits
     setYear(value);
   };
-  
   const handleKmInput = (event) => {
-    const value = event.target.value.replace(/\D/g,'');
+    const value = event.target.value.replace(/\D/g, '');
     setKm(value);
+  };
+  const handleSelectedLocation = (event) => {
+    const value = event.target.value
+    setSelectedLocation(value);
   }
 
   const getManufacturerOptions = () => {
@@ -61,43 +106,68 @@ function Homepage() {
 
   return (
     <div>
-      <Form.Select onChange={handleManufacturerSelect}>
-        <option>Select Manufacturer</option>
-        {getManufacturerOptions()}
-      </Form.Select>
-      {selectedManufacturer && (
-        <Form.Select onChange={handleModelSelect}>
-          <option>Select Model</option>
-          {getModelOptions()}
+      <Form onSubmit={handleSubmit}>
+        <Form.Select onChange={handleManufacturerSelect}>
+          <option>Select Manufacturer</option>
+          {getManufacturerOptions()}
         </Form.Select>
-      )}
-      {
-        <Form.Select onChange={handleTransmissionSelect}>
-            <option>Select Transmission</option>
-            <option value="automatic">Automatic</option>
-            <option value="manual">Manual</option>
+        {selectedManufacturer && (
+          <Form.Select onChange={handleModelSelect}>
+            <option>Select Model</option>
+            {getModelOptions()}
           </Form.Select>
-      }
-      
-      {selectedModel && (
-        <Form.Control
-          type="text"
-          placeholder="Enter year (integer only)"
-          value={year}
-          onChange={handleYearInput}
-        />
-      )}
-      {selectedModel && (
-        <Form.Control
-          type = "text"
-          placeholder="Enter Kilmeter driven (integer only)"
-          value={km}
-          onChange={handleKmInput}
-        />
-      )}
+        )}
+        {
+          <Form.Select onChange={handleTransmissionSelect}>
+            <option>Select Transmission</option>
+            <option value={selectedTransmission}>Automatic</option>
+            <option value={selectedTransmission}>Manual</option>
+          </Form.Select>
+        }
+        {
+          <Form.Select onChange={handleFuelSelect}>
+            <option>Select Fuel Type</option>
+            <option value={selectedFuel}>Petrol</option>
+            <option value={selectedFuel}>Diesel</option>
+          </Form.Select>
+        }
+        {
+          <Form.Select onChange={handleOwnershipSelect}>
+            <option>Select Ownership</option>
+            <option value={selectedOwnership}>1</option>
+            <option value={selectedOwnership}>2</option>
+            <option value={selectedOwnership}>3</option>
+          </Form.Select>
+        }
+
+        {selectedModel && (
+          <Form.Control
+            type="text"
+            placeholder="Enter year (integer only)"
+            value={year}
+            onChange={handleYearInput}
+          />
+        )}
+        {selectedModel && (
+          <Form.Control
+            type="text"
+            placeholder="Enter Kilmeter driven (integer only)"
+            value={km}
+            onChange={handleKmInput}
+          />
+        )}
+        {selectedModel && (
+          <Form.Control
+            type="text"
+            placeholder="Location"
+            value={selectedLocation}
+            onChange={handleSelectedLocation}
+          />
+        )}
+        <Button as="input" type="submit" value="Submit" />{' '}
+      </Form>
     </div>
   );
-
 }
 
-export default Homepage
+  export default Homepage
