636a6939c4f058e60348fb5c6604a76742aa1f7e
Nimit Kumar <nimitkumar2002@gmail.com>
2023-04-19 21:00:44 +0530

    homepage almost made

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,55 +1,100 @@
 import React, { useState } from "react";
 import { carData } from "../data/Appdata";
-import Dropdown from 'react-bootstrap/Dropdown';
+import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Homepage() {
-    const [selectedManufacturer, setSelectedManufacturer] = useState('');
+  const [selectedManufacturer, setSelectedManufacturer] = useState('');
   const [selectedModel, setSelectedModel] = useState('');
+  const [year, setYear] = useState('');
+  const [km, setKm] = useState('');
+  const [selectedTransmission, setSelectedTransmission] = useState('');
 
-  const handleManufacturerChange = (e) => {
-    setSelectedManufacturer(e);
-    setSelectedModel(''); // reset selected model
+  const handleManufacturerSelect = (event) => {
+    setSelectedManufacturer(event.target.value);
+    setSelectedModel('');
+    setSelectedTransmission('');
   };
 
-  const handleModelChange = (e) => {
-    setSelectedModel(e);
+  const handleTransmissionSelect = (event) => {
+    setSelectedTransmission(event.target.value);
   };
+  const handleModelSelect = (event) => {
+    setSelectedModel(event.target.value);
+    setSelectedTransmission('');
+  };
+  const handleYearInput = (event) => {
+    const value = event.target.value.replace(/\D/g, ''); // only allow digits
+    setYear(value);
+  };
+  
+  const handleKmInput = (event) => {
+    const value = event.target.value.replace(/\D/g,'');
+    setKm(value);
+  }
 
-  const manufacturerOptions = carData.map((data) => (
-    <Dropdown.Item key={data.manufacturer} eventKey={data.manufacturer}>
-      {data.manufacturer}
-    </Dropdown.Item>
-  ));
-
-  const modelOptions =
-    selectedManufacturer &&
-    carData.find((data) => data.manufacturer === selectedManufacturer).models.map((model) => (
-      <Dropdown.Item key={model} eventKey={model}>
-        {model}
-      </Dropdown.Item>
+  const getManufacturerOptions = () => {
+    return carData.map((item, index) => (
+      <option key={index} value={item.manufacturer}>
+        {item.manufacturer}
+      </option>
     ));
+  };
+
+  const getModelOptions = () => {
+    if (!selectedManufacturer) {
+      return null;
+    }
+
+    const manufacturer = carData.find(item => item.manufacturer === selectedManufacturer);
+    if (!manufacturer) {
+      return null;
+    }
+
+    return manufacturer.models.map((model, index) => (
+      <option key={index} value={model}>
+        {model}
+      </option>
+    ));
+  };
 
   return (
     <div>
-      <h2>Select your car</h2>
-      <Dropdown onSelect={handleManufacturerChange}>
-        <Dropdown.Toggle variant="primary" id="manufacturer-dropdown">
-          {selectedManufacturer || 'Manufacturer'}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>{manufacturerOptions}</Dropdown.Menu>
-      </Dropdown>
-      <Dropdown onSelect={handleModelChange}>
-        <Dropdown.Toggle
-          variant="primary"
-          id="model-dropdown"
-          disabled={!selectedManufacturer}
-        >
-          {selectedModel || 'Model'}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>{modelOptions}</Dropdown.Menu>
-      </Dropdown>
+      <Form.Select onChange={handleManufacturerSelect}>
+        <option>Select Manufacturer</option>
+        {getManufacturerOptions()}
+      </Form.Select>
+      {selectedManufacturer && (
+        <Form.Select onChange={handleModelSelect}>
+          <option>Select Model</option>
+          {getModelOptions()}
+        </Form.Select>
+      )}
+      {
+        <Form.Select onChange={handleTransmissionSelect}>
+            <option>Select Transmission</option>
+            <option value="automatic">Automatic</option>
+            <option value="manual">Manual</option>
+          </Form.Select>
+      }
+      
+      {selectedModel && (
+        <Form.Control
+          type="text"
+          placeholder="Enter year (integer only)"
+          value={year}
+          onChange={handleYearInput}
+        />
+      )}
+      {selectedModel && (
+        <Form.Control
+          type = "text"
+          placeholder="Enter Kilmeter driven (integer only)"
+          value={km}
+          onChange={handleKmInput}
+        />
+      )}
     </div>
   );
 
