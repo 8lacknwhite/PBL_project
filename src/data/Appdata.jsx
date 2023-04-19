f9c20df8ebe9a7811cd4c0b9eec2ba932f04c4df
Nimit Kumar <nimitkumar2002@gmail.com>
2023-04-19 13:15:00 +0530

    made data folder for the dropdown menu data started adding

---
+++ src/data/Appdata.jsx
@@ -0,0 +1,5 @@
+export const carData= [
+    {manufacturer:'Maruti', models: ['Swift', 'Celerio', 'Baleno', 'Alto', 'Wagon R', 'Zen Estilo', 'A-Star', 'Vitara Brezza', 'Swift Dzire','Ertiga', 'Ritz', 'Eeco', 'S Cross', 'S-Presso', 'Ignis', 'Ciaz', ]},
+    {manufacturer: 'Volkswagen', models: ['Ameo', 'Polo', 'Vento', 'Jetta', 'Passat']},
+    {manufacturer: 'Ford', models:['Figo', 'Eco Sport', 'Feista', 'Figo Aspire']},
+]
--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,15 +1,3 @@
 import React, { useState } from "react";
 import { Dropdown } from "react-bootstrap";
 
-const options = {
-    'A': [X,Y,Z],
-    'B': [J,K,L],
-    'C': [M,N,O]
-}
-
-const Homepage = () =>{
-    const [selectedOption,setSelectedOption] = useState('')
-    return(
-
-    )
-}
